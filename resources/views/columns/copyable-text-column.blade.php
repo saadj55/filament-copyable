50c70038a9f6eeb4e19fa2f14ce1cae7cc8fbff2
cobraprojects <42706279+cobraprojects@users.noreply.github.com>
2022-07-18 08:14:29 +0200

    change style to match with the design

--- resources/views/columns/copyable-text-column.blade.php
+++ resources/views/columns/copyable-text-column.blade.php
@@ -1,6 +1,6 @@
 <div
         {{ $attributes->merge($getExtraAttributes())->class([
-            'px-4 py-3 filament-tables-text-column',
+            'px-4 py-3 filament-tables-text-column flex items-center gap-2',
             'text-primary-600 transition hover:underline hover:text-primary-500 focus:underline focus:text-primary-500' => $getAction() || $getUrl(),
             'whitespace-normal' => $canWrap(),
         ]) }}
@@ -9,7 +9,7 @@
     {{ $getFormattedState() }}
 
     <button @click.prevent="$clipboard( '{{$getFormattedState()}}')">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-primary-700 dark:text-primary-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
         </svg>
     </button>
